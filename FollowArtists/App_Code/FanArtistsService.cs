b4a21a24dd05fa38929763282b8b7086570c5552
Anna <atiagina@gmail.com>
2016-03-16 12:33:24 -0700

    Adds service for Fans to follow artists
    Displaying the artists and their shows is commented for now

---
+++ FollowArtists/App_Code/FanArtistsService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "FanArtistsService" in code, svc and config file together.
+public class FanArtistsService : IFanArtistsService
+{
+    ShowTrackerEntities ste = new ShowTrackerEntities();
+    public int FollowArtist(int fkey, string aname)
+    {
+        int result = 1;
+
+        Fan newFan = (from f in ste.Fans
+                      where f.FanKey.Equals(fkey)
+                      select f).First();
+
+        Artist newArtist = (from a in ste.Artists
+                            where a.ArtistName.Equals(aname)
+                            select a).First();
+
+        newFan.Artists.Add(newArtist);
+
+        ste.SaveChanges();
+
+        return result;
+    }
+
+
+ /*
+    public List<ShowInfo> GetShowsForFanArtists(int fanKey)
+    {
+        Fan myFan = (from f in ste.Fans
+                     where f.FanKey == fanKey
+                     select f).First();
+
+        List<ShowInfo> shows = new List<string>();
+
+        
+        foreach (Artist a in myFan.Artists)
+        {
+           
+            var shws = from s in ste.Shows
+                       from sd in s.ShowDetails
+                       where sd.ArtistKey == a.ArtistKey
+                       select new
+                       {
+                           s.ShowName,
+                           s.ShowTime,
+                           s.ShowDate,
+                           s.ShowTicketInfo,
+                           s.Venue.VenueName,
+                           sd.Artist.ArtistName
+                       };
+
+            foreach (var sh in shws)
+            {
+                ShowInfo info = new ShowInfo();
+                info.ShowName = sh.ShowName;
+                info.ShowDate = sh.ShowDate;
+                info.ShowTime = sh.ShowTime;
+                info.ShowTicket = sh.ShowTicketInfo;
+                info.VenueName = sh.VenueName;
+                info.ArtistName = sh.ArtistName;
+
+                shows.Add
+            }
+
+        }
+        return shows;
+
+    }
+    */
+}
---
+++ FollowArtists/App_Code/IFanArtistsService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFanArtistsService" in both code and config file together.
+[ServiceContract]
+public interface IFanArtistsService
+{
+    [OperationContract]
+    int FollowArtist(int fkey, string aname);
+
+   /* [OperationContract]
+    List<ShowInfo> GetShowsForFanArtists(int fanKey); */
+}
+
+/*
+[DataContract]
+
+public class ShowInfo
+{
+    [DataMember]
+    public string ShowName { set; get; }
+
+    [DataMember]
+    public DateTime ShowDate { set; get; }
+
+    [DataMember]
+    public TimeSpan ShowTime { set; get; }
+
+    [DataMember]
+    public string ShowTicket { set; get; }
+
+    [DataMember]
+    public int VenueKey { set; get; }
+
+    [DataMember]
+    public string ArtistName { set; get; }
+
+    [DataMember]
+    public string VenueName { set; get; }
+
+
+}
+ */
